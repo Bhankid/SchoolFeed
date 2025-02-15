a04dce8ce9f91745623c3492fe0715f2739a01c3
Bhankid <alfredfianyo50@gmail.com>
2025-02-15 21:20:35 +0000

    Add ProtectedRoute for securing the Home page; refactor App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext"; // Ensure AuthProvider is wrapping everything
+import ProtectedRoute from "./routes/ProtectedRoute"; // Import ProtectedRoute
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import Home from "./Home";
 import Logout from "./pages/Logout";
 
-function App() {
+const App: React.FC = () => {
   return (
     <AuthProvider>
-      {" "}
-      {/* Wrap the entire application */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />
-          <Route path="/home" element={<Home />} />
+          {/* Protect the /home route */}
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
   );
-}
+};
 
 export default App;
